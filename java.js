a0779101baebbc5712157b2ee2c6f8e133224b0a
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-07-22 02:27:31 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,30 @@
+let form = document.querySelector("form")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+    let height = parseInt(document.getElementById("input-height").value)
+    let weight = parseInt(document.getElementById("input-weight").value)
+    let show = document.getElementById("show")
+
+    if(height < 0) {
+        show.textContent = `Please enter valid height`;
+    } else if (weight < 0) {
+        show.textContent = `Please enter valid weight`
+    } else {
+        let bmiChecker = (weight / height).toFixed(2)
+        show.textContent = bmiChecker
+    }
+})
+
+document.getElementById("refresh-weight").addEventListener("click", () => {
+    inputWeight.value = ""
+})
+
+document.getElementById("refresh-height").addEventListener("click", () => {
+    inputHeight.value = ""
+})
+
+document.getElementById("refresh-result").addEventListener("click", () => {
+    show.innerText = "0"
+})
